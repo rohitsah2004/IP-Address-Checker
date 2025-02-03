0d8c3fdb2dec5cb513061f36336bc9a4facc3ccc
rohitsah2004 <rsah@my.tnstate.edu>
2025-02-03 14:48:27 -0600

    Add files via upload

---
+++ IP_Checker.py
@@ -0,0 +1,12 @@
+import socket
+
+def get_ip_address(domain):
+    try:
+        ip_address = socket.gethostbyname(domain)
+        print(f"The IP address of {domain} is: {ip_address}")
+    except socket.gaierror:
+        print("Invalid domain name or network error.")
+
+if __name__ == "__main__":
+    domain = input("Enter the domain name: ")
+    get_ip_address(domain)
